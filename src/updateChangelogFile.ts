dbd59df8c9f0d8fd5c2874b25753da23490fbc00
Samuel Johnson <samj65464@gmail.com>
2020-10-14 20:56:33 +0100

    other: Linted and formatted

--- src/updateChangelogFile.ts
+++ src/updateChangelogFile.ts
@@ -1,9 +1,7 @@
 import { info, getInput } from "@actions/core";
 import * as fs from "fs";
 
-export async function updateChangelogFile(
-  changeLog: string,
-): Promise<void> {
+export async function updateChangelogFile(changeLog: string): Promise<void> {
   let changeLogFilePath = getInput("changeLogFilePath", { required: false });
   if (changeLogFilePath.length === 0) changeLogFilePath = "./CHANGELOG.md";
   info(`Updating changelog file at ${changeLogFilePath}`);
@@ -12,26 +10,42 @@ export async function updateChangelogFile(
   if (fileHeading.length === 0) fileHeading = "# Changelog";
 
   let sectionHeading = getInput("sectionHeading", { required: false });
-  if (sectionHeading.length === 0) sectionHeading = `## Release ${process.env.GITHUB_REF}`;
+  if (sectionHeading.length === 0)
+    sectionHeading = `## Release ${process.env.GITHUB_REF}`;
 
   let existingContent = "";
   if (await fs.promises.stat(changeLogFilePath)) {
-    existingContent = (await fs.promises.readFile(changeLogFilePath)).toString();
+    existingContent = (
+      await fs.promises.readFile(changeLogFilePath)
+    ).toString();
   }
 
-  const updatedContent = createNewContent(existingContent, changeLog, fileHeading, sectionHeading);
+  const updatedContent = createNewContent(
+    existingContent,
+    changeLog,
+    fileHeading,
+    sectionHeading,
+  );
 
-  info(`Writing new or updated changelog file`);
+  info("Writing new or updated changelog file");
   await fs.promises.writeFile(changeLogFilePath, updatedContent);
 }
 
-function createNewContent(existingContent: string, newContent: string, fileHeading: string, sectionHeading: string): string {
+function createNewContent(
+  existingContent: string,
+  newContent: string,
+  fileHeading: string,
+  sectionHeading: string,
+): string {
   let updatedContent = "";
   if (existingContent.length === 0) {
-    updatedContent = `${fileHeading}\r\r${addNewReleaseSection(newContent, sectionHeading)}`;
+    updatedContent = `${fileHeading}\r\r${addNewReleaseSection(
+      newContent,
+      sectionHeading,
+    )}`;
   } else {
     // Remove original heading so we can add our new section then add it back
-    const strippedContent = existingContent.replace(fileHeading, '').trim();
+    const strippedContent = existingContent.replace(fileHeading, "").trim();
 
     const releaseSection = addNewReleaseSection(newContent, sectionHeading);
     updatedContent = `${fileHeading}\r\r${releaseSection}${strippedContent}`;
