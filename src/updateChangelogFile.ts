ef666fb26e89ef551b5c187250c847cd5b374a91
Samuel Johnson <samj65464@gmail.com>
2020-10-18 21:15:28 +0100

    fix: Linted and rebuilt action

--- src/updateChangelogFile.ts
+++ src/updateChangelogFile.ts
@@ -33,7 +33,7 @@ export async function updateChangelogFile(
     path: changeLogPath,
     content: updatedContent,
     owner: context.repo.owner,
-    message: `chore: updating change log [skip ci]`,
+    message: "chore: updating change log [skip ci]",
     author: {
       name: "GitHub CI",
       email: "ci@github.com",
@@ -49,17 +49,24 @@ function createNewContent(
 ): string {
   let updatedContent = "";
   if (existingContent.length === 0) {
-    updatedContent = `${title}\n\n${addNewReleaseSection(sectionHeading, newContent)}`;
+    updatedContent = `${title}\n\n${addNewReleaseSection(
+      sectionHeading,
+      newContent,
+    )}`;
   } else {
     const releaseSection = addNewReleaseSection(sectionHeading, newContent);
 
-    // Find last release heading which will be a level 2 head: '## '
-    const lastReleaseIndex = existingContent.indexOf('\n## ');
+    // Find last release heading which will be a level 2 head
+    const lastReleaseIndex = existingContent.indexOf("\n## ");
     if (lastReleaseIndex === -1) {
       // Should never get here really, but if we do append the new changelog to the end
       updatedContent = `${existingContent}\n\n${releaseSection}`;
     } else {
-      updatedContent = `${existingContent.substr(0, lastReleaseIndex).trim()}\n\n${releaseSection}${existingContent.substr(lastReleaseIndex).trim()}`;
+      updatedContent = `${existingContent
+        .substr(0, lastReleaseIndex)
+        .trim()}\n\n${releaseSection}${existingContent
+        .substr(lastReleaseIndex)
+        .trim()}`;
     }
   }
 
